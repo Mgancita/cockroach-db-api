9f695db97c80b3190f95a4b9369305d4c724c165
Marco Gancitano <marco.gancitano97@gmail.com>
2022-03-18 16:09:44 -0400

    Make black compliant

--- v1/schemas/students.py
+++ v1/schemas/students.py
@@ -7,6 +7,7 @@ import uuid
 from pydantic import BaseModel, EmailStr, Field, UUID4
 from v1.schemas import PropertyBaseModel
 
+
 class StudentBase(BaseModel):
     """Base schema for Students."""
 
